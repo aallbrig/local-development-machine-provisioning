f73e0edc46d5d73d0b0667da6157f69de4615057
Andrew Allbright <aallbrig@gmail.com>
2017-01-29 13:23:33 -0500

    Bring in a couple basic files from v1 repo, implement different run strategy.

---
+++ run.sh
@@ -0,0 +1,33 @@
+function setup_osx {
+  echo "osx"
+
+  # Prerequisites
+  pip --version
+  if [[ $? != 0 ]] ; then
+    echo "No valid pip installed.  Installing..."
+    sudo easy_install pip
+  fi
+  ansible --version
+  if [[ $? != 0 ]] ; then
+    echo "No valid ansible installed.  Installing..."
+    sudo pip install ansible
+  fi
+  ansible-galaxy install -r provisioning/requirements.yml
+  if [[ $? != 0 ]] ; then
+    echo "Try \`sudo ansible-galaxy install -r provisioning/requirements.yml\`"
+    exit 0
+  fi
+
+  # Primary ansible entrypoint
+  ansible-playbook provisioning/setup-machine.yml --ask-sudo-pass
+  exit 0
+}
+
+case "$OSTYPE" in
+  solaris*) echo "SOLARIS" ;;
+  darwin*) setup_osx ;;
+  linux*) echo "LINUX" ;;
+  bsd*) echo "BSD" ;;
+  msys*) echo "windows" ;;
+  *) echo "unknown: $OSTYPE" ;;
+esac
